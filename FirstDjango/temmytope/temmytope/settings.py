787cec22c9b035080f821a0a82726b9bbce75047
temmytope88 <fakiletemitope@gmail.com>
2022-06-01 04:29:47 +0100

    remove secret key

--- FirstDjango/temmytope/temmytope/settings.py
+++ FirstDjango/temmytope/temmytope/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7idne#u8!im&qp*3fmg*aqj&65ss&)6cr)h8nmnvj*=+7idn&-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
